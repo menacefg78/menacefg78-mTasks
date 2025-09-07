419b02e28841abbbfbbaed4867e29d35aaa5e228
menacefg78 <158286807+menacefg78@users.noreply.github.com>
2025-09-07 09:07:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/menacefg78-mTasks",
+  base: "/menacefg78-mTasks/",
 })
